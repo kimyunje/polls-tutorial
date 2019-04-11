0c972151dcfa1f84f19d44a6dc22ffe277d108be
kimyunje <stoneaxe002@gmail.com>
2019-04-11 17:16:08 +0900

    새로운 유저에게 questions만을 추가할 수 있는 권한을 준다.

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import django.contrib.auth
+LOGIN_URL = '/login/'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
