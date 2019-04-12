77adcfe918e6d5bd33a92a766b2312cca01c162d
kimyunje <stoneaxe002@gmail.com>
2019-04-12 14:37:25 +0900

    투표 기능 추가

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 
-from .models import Question
+from .models import Choice, Question
 from django.contrib.auth.decorators import login_required, permission_required
 
-# @permission_required('questions.add_question_text', login_url="/admin/login/")
-@login_required
+from django.urls import reverse
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
@@ -16,8 +16,23 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "you're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+def add_question(request):
+    question = get_object_or_404(Question, pk=question_id)
+    add_question = question.question_text_set.get(pk=request.POST['question_text'])
+    return render(request, 'polls/add_question.html', {})
